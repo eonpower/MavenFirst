bdaaf2c3aecfae8935f21acebdf521ac7963c64e
eon <eonpower2@gmail.com>
2019-06-10 11:18:48 +0200

    first commit

---
+++ MavenTest/src/main/java/servlet/Description.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bean.*;
+
+public class Description extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Description() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		//send a bean object
+				Pesonne personne = new Pesonne();		
+				request.setAttribute("auteur", "Description");
+				
+				
+				this.getServletContext().getRequestDispatcher("/WEB-INF/decription.jsp").forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ MavenTest/src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"   pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>SoftBint</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+ 
+ 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+ 
+  
+<link rel="shortcut icon" href="http://chittagongit.com/download/41532" />
+</head>
+
+<body>
+
+	<h1>Welcome to index</h1>
+
+</body>
+</html>
+ 
